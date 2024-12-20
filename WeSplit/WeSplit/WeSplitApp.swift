2d6ceedefa50758b9154396eb8ed491746ffc0fc
Evgen Designer <69058813+evgen-designer@users.noreply.github.com>
2024-12-20 14:42:33 +0000

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mac on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
